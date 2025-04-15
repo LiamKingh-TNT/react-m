5971fbaa435a4e39fa198c49e09a228b996e5ad1
LiamKingh-TNT <commandblock365@gmail.com>
2025-04-16 05:57:46 +0800

    update

--- src/components/product_list.jsx
+++ src/components/product_list.jsx
@@ -10,17 +10,24 @@ function ProductList() {
             <p className="text-[#3E2F2F] z-50 mt-[-10vh] title overflow-visible text-[8vw] font-extrabold" data-storke={lang('title2')}>{lang('title2')}</p>
           </div>
           <div className="background pt-20 relative z-[0] w-[100vw] h-full mt-[0vh] mx-auto backdrop-blur-md shadow-md md:w-[90vw] overflow-hidden">
-            <div className="brief_desc w-full h-[80vh]">
+            <div className="brief_desc w-full h-[80vh] grid grid-cols-1 md:grid-cols-2">
               <img
-                src="/images/Lords.png"
+                src="/images/lords.png"
                 alt="masked"
                 className="h-full w-full object-contain"
                 style={{
-                  maskImage: 'radial-gradient(ellipse at center, rgba(0,0,0,1) 60%, rgba(0,0,0,0) 75%)',
-                  WebkitMaskImage: 'radial-gradient(ellipse at center, rgba(0,0,0,1) 60%, rgba(0,0,0,0) 80%)'
+                  maskImage: 'radial-gradient(circle at center, rgba(0,0,0,1) 40%, rgba(0,0,0,0) 60%)',
+                  WebkitMaskImage: 'radial-gradient(circle at center, rgba(0,0,0,1) 40%, rgba(0,0,0,0) 60%)'
                 }}
               />
+              <div className="flex flex-col justify-center items-start px-6">
+                <p className="text-white text-xl">
+                  12325456
+                </p>
+              </div>
             </div>
+            <br/>
+            <br/>
           </div>
         </div>
       
