d1be00d86c119b18ae0616b214771d7163dc16e5
LiamKingh-TNT <commandblock365@gmail.com>
2025-04-17 00:00:28 +0800

    push

--- src/assets/factions.js
+++ src/assets/factions.js
@@ -1,11 +1,17 @@
 import the_supreme_empire from './factions/the_supreme_empire.json'
 import ecclesia_of_luca from './factions/ecclesia_of_luca.json'
 import octorian_vanguard from './factions/octorian_vanguard.json'
+import dwarf_forge from './factions/dwarf_forge.json'
+import alliance_of_ascendants from './factions/alliance_of_ascendants.json'
+import crimson_sunchasers from './factions/crimson_sunchasers.json'
 
 const factions = [
     the_supreme_empire,
     ecclesia_of_luca,
-    octorian_vanguard
+    octorian_vanguard,
+    dwarf_forge,
+    alliance_of_ascendants,
+    crimson_sunchasers
 ]
 
 export default factions
