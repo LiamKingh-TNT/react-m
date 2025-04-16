91ec155cd2661c7d7d6b40a0768af216e43a23d1
jxex <chin45106@gmail.com>
2025-04-16 20:13:06 +0800

    add story and rule page

---
+++ src/pages/story.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/header";
+import Footer from "../components/footer"
+
+function Story(){
+    return(
+        <div className="container mx-auto max-w-none w-full px-0">
+            <Header/>
+            <img src="./images/home_cover.png" className="fixed z-0 top-0 h-screen w-screen object-cover"/>
+         </div>
+    )
+}
+
+export default Story;
