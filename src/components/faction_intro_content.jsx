f0a763646a3d9ed94a7d89961b5e5ae2589268db
LiamKingh-TNT <commandblock365@gmail.com>
2025-04-18 02:20:17 +0800

    update

--- src/components/faction_intro_content.jsx
+++ src/components/faction_intro_content.jsx
@@ -2,16 +2,14 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router';
 import { useState, useEffect, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import { gsap } from 'gsap';
+import { InfoBoard } from './info_board';
 import TitleText from './title_text'
 export function FactionIntroContent(faction) {
     const rom_number = ['O','I','II','III','IV','V','VI','VII','VIII','IX','X'];
     const { t:lang } = useTranslation();
     const [blur, setBlur] = useState(0);
     const [selected_trait, SetSelectedTrait] = useState(0);
-    const [selected_lord, SetSelectedLord] = useState(0);
-    const [lord_info,SetLordInfo] = useState(0);
     const btnRefs = useRef([]);
-    
 
     const handleMouseEnter = (index) => {
       gsap.to(btnRefs.current[index], {
@@ -49,7 +47,6 @@ export function FactionIntroContent(faction) {
           window.scrollTo({ top: y, behavior: 'smooth' });
         }
       };
-    console.log(faction)
     return(
         <div className="content relative z-20 overflow-x-hidden">
           <div className="flex items-center justify-center h-screen">
@@ -118,113 +115,21 @@ export function FactionIntroContent(faction) {
                     })}
                 </div> 
               </div>
-              <div  id="lords" className="h-fit w-[100vw] mt-[250px] mx-auto md:mt-[150px]">
-                    <div className="flex items-center justify-center">
-                      <TitleText
-                        text={lang('fi.lords')}
-                        fontSize={80}
-                        strokeWidth={30}
-                        fill="#3E2F2F"
-                        strokeColors={["#A0C5CC", "#ffffff", "#A0C5CC"]}
-                        width={1000}
-                        height={1000}
-                      />
-                    </div>
-                    <div className="info_frame max-h-fit min-h-[25vh] md:min-h-[50vh] w-[100vw] absolute right-0 md:mr-[16px] md:w-[85vw] grid grid-cols-1 grid-rows-3 md:grid-cols-10 md:grid-rows-5 overflow-visible md:border-y-8">
-
-                      <div className="col-span-3 row-span-5 relative overflow-visible z-0">
-                        <img
-                          src={faction.faction.lords[selected_lord].img}
-                          className="absolute bottom-0 -top-[30vh] w-auto h-[80vh] object-contain"
-                          style={{
-                            maskImage: `radial-gradient(circle at center, rgba(0,0,0,1) 40%, rgba(0,0,0,0) 80%)`,
-                            WebkitMaskImage: `radial-gradient(circle at center, rgba(0,0,0,1) 40%, rgba(0,0,0,0) 80%)`,
-                          }}
-                        />
-                      </div>
-
-                      <div className="col-span-6 row-span-5 flex flex-col items-start pl-1 pt-1">
-                        <TitleText
-                          text={lang(faction.faction.lords[selected_lord].name)}
-                          fontSize={40}
-                          strokeWidth={10}
-                          fill="#B5D7D7"
-                          strokeColors={["#A0C5CC", "#ffffff", "#A0C5CC"]}
-                          width={1000}
-                          height={1000}
-                          align="left"
-                        />
-                        <div className="text-[25px] md:text-[30px] text-white text-left mt-0 z-20">
-                          {lord_info===0 &&(
-                            <p className=" text-white">
-                              {lang(faction.faction.lords[selected_lord].story)}
-                            </p>
-                          )}
-                          {lord_info===1&&(
-                            <div className="grid grid-cols-5 w-full h-fit">
-                              <div className="col-span-2 text-right text-[#C4C4C4]">
-                                <p>{lang('fi.hp')}</p>
-                                <p>{lang('fi.def')}</p>
-                                <p>{lang('fi.dex')}</p>
-                                <p>{lang('fi.size')}</p>
-                                <p>{lang('fi.type')}</p>
-                              </div>
-                              <div className="col-span-3 ml-2">
-                                <p>{lang(faction.faction.lords[selected_lord].hp)}</p>
-                                <p>{lang(faction.faction.lords[selected_lord].def)}</p>
-                                <p>{lang(faction.faction.lords[selected_lord].dex)}</p>
-                                <p>{lang(faction.faction.lords[selected_lord].size)}</p>
-                                <p>{lang(faction.faction.lords[selected_lord].type)}</p>
-                                
-                              </div>
-                            </div>
-                          )}
-                          {lord_info===2&&(
-                            <div className="flex flex-col w-full h-fit">
-                              {faction.faction.lords[selected_lord].skills.map((skill, index) => (
-                                <div key={lang(skill.name)} className="grid grid-cols-5 w-full h-fit">
-                                  <div className="col-span-2 text-right text-[#C4C4C4]">
-                                    <p>{lang(skill.name)}</p>
-                                  </div>
-                                  <div className="col-span-3 grid grid-rows-2 ml-2">
-                                    <p>{lang(skill.desc)}</p>
-                                    <div className="flex items-start w-full h-fit">
-                                      <p  className="text-right z-40 text-[#C4C4C4]">{lang('fi.cost')}</p>
-                                      <p  className="text-left">{lang(skill.cost)}</p>
-                                    </div>
-                                  </div>
-                                </div>
-                              ))}
-                            </div>
-                          )}
-                        </div>
-                      </div>
-                      <div className="absolute top-80 md:top-1/2 right-0 -translate-y-1/2 flex flex-col space-y-0 z-50">
-                        <button onClick={()=>SetLordInfo(0)} className={`w-16 h-16 bg-[${lord_info==0?"#7FA4AA":"#44646B"}] rounded-l-md flex items-center justify-center`}>
-                          <img src="/images/open-book.png" alt="icon" className="w-14 h-auto" />
-                        </button>
-                        <button onClick={()=>SetLordInfo(1)} className={`w-16 h-16 bg-[${lord_info==1?"#7FA4AA":"#44646B"}] rounded-l-md flex items-center justify-center`}>
-                          <img src="/images/features.png" alt="icon" className="w-14 h-auto" />
-                        </button>
-                        <button onClick={()=>SetLordInfo(2)} className={`w-16 h-16 bg-[${lord_info==2?"#7FA4AA":"#44646B"}] rounded-l-md flex items-center justify-center`}>
-                          <img src="/images/skill.png" alt="icon" className="w-14 h-auto" />
-                        </button>
-                      </div>
-                      <div className="w-[60vw] bg-amber-800 h-fit absolute -bottom-30 left-[30vw] flex flex-wrap items-start justify-start gap-3">
-                        <div className="bg-amber-50 w-[6em] h-[6em]"></div>
-                        <div className="bg-amber-300 w-[6em] h-[6em]"></div>
-                      </div>
-                    </div>
-              </div>
+              
+              <InfoBoard faction={faction.faction} type="lord"/>
+              <div className="mt-[200vh] md:mt-[100vh]"/>
+              <InfoBoard faction={faction.faction} type="soldier"/>
+              <div className="mt-[200vh] md:mt-[100vh]"/>
+              <InfoBoard faction={faction.faction} type="card"/>
               <div className="mt-[200vh] md:mt-[150vh]"/>
               <div className="hidden md:flex flex-col fixed left-0 top-1/2 mt-15 ml-10 -translate-y-1/2 items-center justify-center w-[10vw]">
                 <button onClick={()=>scrollToWithOffset("traits")} className=" text-white text-[25px]">{lang('fi.traits')}</button>
                 <div className="h-10 w-[2px] bg-gray-300" />
                 <button onClick={()=>scrollToWithOffset("lords")} className=" text-white text-[25px]">{lang('fi.lords')}</button>
                 <div className="h-10 w-[2px] bg-gray-300" />
-                <button onClick={()=>scrollToWithOffset("traits")} className=" text-white text-[25px]">{lang('fi.traits')}</button>
+                <button onClick={()=>scrollToWithOffset("soldiers")} className=" text-white text-[25px]">{lang('fi.soldiers')}</button>
                 <div className="h-10 w-[2px] bg-gray-300" />
-                <button onClick={()=>scrollToWithOffset("traits")} className=" text-white text-[25px]">{lang('fi.traits')}</button>
+                <button onClick={()=>scrollToWithOffset("cards")} className=" text-white text-[25px]">{lang('fi.cards')}</button>
               </div>
 
             </div>
---
+++ src/components/info_board.jsx
@@ -0,0 +1,134 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router';
+import { useState} from 'react'
+import { useTranslation } from 'react-i18next'
+import TitleText from './title_text'
+export function InfoBoard({faction, type="lord"}){
+    console.log(faction)
+    const { t:lang } = useTranslation();
+    const [selected, SetSelected] = useState(0);
+    const [info,SetInfo] = useState(0);
+    const obj =
+        type=='lord'?
+        faction.lords:
+        type=='soldier'?
+        faction.soldiers:
+        faction.cards
+    return(
+        <div  id={`${type}s`} className="h-fit w-[100vw] mt-[250px] mx-auto md:mt-[150px]">
+            <div className="flex items-center justify-center">
+                <TitleText
+                text={lang(`fi.${type}s`)}
+                fontSize={80}
+                strokeWidth={30}
+                fill="#3E2F2F"
+                strokeColors={["#A0C5CC", "#ffffff", "#A0C5CC"]}
+                width={1000}
+                height={1000}
+                />
+            </div>
+            <div className="info_frame min-h-fit md:min-h-[50vh] w-[100vw] absolute right-0 md:mr-[16px] md:w-[85vw] grid grid-cols-1 grid-rows-3 md:grid-cols-10 md:grid-rows-5 overflow-visible md:border-y-8">
+
+                <div className="col-span-3 row-span-5 relative overflow-visible z-0">
+                <img
+                    src={obj[selected].img}
+                    className="absolute bottom-0 -top-[30vh] w-auto h-[80vh] object-contain"
+                    style={{
+                    maskImage: `radial-gradient(circle at center, rgba(0,0,0,1) 40%, rgba(0,0,0,0) 80%)`,
+                    WebkitMaskImage: `radial-gradient(circle at center, rgba(0,0,0,1) 40%, rgba(0,0,0,0) 80%)`,
+                    }}
+                />
+                </div>
+
+                <div className="col-span-6 row-span-5 flex flex-col items-start pl-1 pt-1">
+                <TitleText
+                    text={lang(obj[selected].name)}
+                    fontSize={40}
+                    strokeWidth={10}
+                    fill="#B5D7D7"
+                    strokeColors={["#A0C5CC", "#ffffff", "#A0C5CC"]}
+                    width={1000}
+                    height={1000}
+                    align="left"
+                />
+                <div className="text-[25px] md:text-[30px] text-white text-left mt-0 z-20">
+                    {type=="card"&&(
+                        <div className="grid grid-cols-1 w-full h-fit">
+                            <p className=" text-white">
+                                {lang(obj[selected].desc)}
+                            </p>
+                            <div className="flex items-start w-full h-fit">
+                                <p  className="text-right z-40 text-[#C4C4C4]">{lang('fi.cost')}</p>
+                                <p  className="text-left">{lang(obj[selected].cost)}</p>
+                            </div>
+                        </div>
+                    )}
+                    {(info===0 && type!="cards")&&(
+                    <p className=" text-white">
+                        {lang(obj[selected].story)}
+                    </p>
+                    )}
+                    {(info===1 && type!="cards")&&(
+                    <div className="grid grid-cols-5 w-full h-fit">
+                        <div className="col-span-2 text-right text-[#C4C4C4]">
+                        <p>{lang('fi.hp')}</p>
+                        <p>{lang('fi.def')}</p>
+                        <p>{lang('fi.dex')}</p>
+                        <p>{lang('fi.size')}</p>
+                        <p>{lang('fi.type')}</p>
+                        {type=="soldier"?<p>{lang('fi.cost')}</p>:<></>}
+                        </div>
+                        <div className="col-span-3 ml-2">
+                        <p>{lang(obj[selected].hp)}</p>
+                        <p>{lang(obj[selected].def)}</p>
+                        <p>{lang(obj[selected].dex)}</p>
+                        <p>{lang(obj[selected].size)}</p>
+                        <p>{lang(obj[selected].type)}</p>
+                        {type=="soldier"?<p>{lang(obj[selected].cost)}</p>:<></>}
+                        
+                        </div>
+                    </div>
+                    )}
+                    {(info===2 && type!="cards")&&(
+                    <div className="flex flex-col w-full h-fit">
+                        {obj[selected].skills.map((skill) => (
+                        <div key={lang(skill.name)} className="grid grid-cols-5 w-full h-fit">
+                            <div className="col-span-2 text-right text-[#C4C4C4]">
+                            <p>{lang(skill.name)}</p>
+                            </div>
+                            <div className="col-span-3 grid grid-rows-2 ml-2">
+                            <p>{lang(skill.desc)}</p>
+                            <div className="flex items-start w-full h-fit">
+                                <p  className="text-right z-40 text-[#C4C4C4]">{lang('fi.cost')}</p>
+                                <p  className="text-left">{lang(skill.cost)}</p>
+                            </div>
+                            </div>
+                        </div>
+                        ))}
+                    </div>
+                    )}
+                </div>
+                </div>
+                {type!="card"? <div className="absolute top-80 md:top-1/2 right-0 -translate-y-1/2 flex flex-col space-y-0 z-50">
+                    <button onClick={()=>SetInfo(0)} className={`w-16 h-16 bg-[${info==0?"#7FA4AA":"#44646B"}] rounded-l-md flex items-center justify-center`}>
+                        <img src="/images/open-book.png" alt="icon" className="w-14 h-auto" />
+                    </button>
+                    <button onClick={()=>SetInfo(1)} className={`w-16 h-16 bg-[${info==1?"#7FA4AA":"#44646B"}] rounded-l-md flex items-center justify-center`}>
+                        <img src="/images/features.png" alt="icon" className="w-14 h-auto" />
+                    </button>
+                    <button onClick={()=>SetInfo(2)} className={`w-16 h-16 bg-[${info==2?"#7FA4AA":"#44646B"}] rounded-l-md flex items-center justify-center`}>
+                        <img src="/images/skill.png" alt="icon" className="w-14 h-auto" />
+                    </button>
+                </div>:<></>}
+                <div className="w-[60vw] h-fit absolute -bottom-30 left-[30vw] flex flex-wrap items-start justify-start gap-3">
+                {obj.map((lord, index) =>{
+                    return(
+                    <button onClick={()=>SetSelected(index)} className={` w-[6em] h-[6em] border-4 border-[${selected==index?"#7FA4AA":"#44646B"}]`}>
+                    <img src={lord.head_img} alt="icon" className="w-full h-full" />
+                    </button>
+                )})}
+                
+                </div>
+            </div>
+        </div>
+    )
+}
