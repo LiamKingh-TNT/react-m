84e1b58405773e538de20a1f5aed3544f673bfd5
LiamKingh-TNT <commandblock365@gmail.com>
2025-04-18 16:04:18 +0800

    NULL

--- src/pages/story.jsx
+++ src/pages/story.jsx
@@ -5,6 +5,7 @@ function Story(){
     return(
         <div className="container mx-auto max-w-none w-full px-0">
             <Header/>
+            <p className="text-[30vw]">NULL</p>
             <img src="./images/home_cover.png" className="fixed z-0 top-0 h-screen w-screen object-cover"/>
          </div>
     )
